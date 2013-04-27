a3e1b433673edb5ca0c2c877c80e155f0401709c
Yann Le Franc <ylefranc@gmail.com>
2013-04-28 00:54:15 +0200

    Add licence info

--- HTBv1_2.m
+++ HTBv1_2.m
@@ -6,9 +6,17 @@
 %                                                                        %
 %       Version 1.2: 11/02/2011                                        	 %
 %                                                                        %
+%	This work is licensed under the Creative Commons Attribution 3.0 	 %
+%	Unported License. To view a copy of this license, visit 			 %
+%	http://creativecommons.org/licenses/by-sa/3.0/ or send a letter to 	 %
+%	Creative Commons, 444 Castro Street, Suite 900, Mountain View, 		 %
+%	California, 94041, USA.												 %
+%																		 %
+%	For questions, suggestions and comments regarding the interface 	 %
+%	and the data format, please contact Y. Le Franc 					 %
+%	(ylefranc(at)gmail.com).         				                     %
 %                                                                        %
-%                                                                        %
-% 
+% 																		 %
 %------------------------------------------------------------------------%
 
 
